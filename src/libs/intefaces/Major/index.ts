fd7c697cd38fc1294f7a13ea3084ea868d2c8458
eungyeole <eungyeole@naver.com>
2021-04-14 22:19:12 +0900

    [UPDATE] MajorData Interface

---
+++ src/libs/intefaces/Major/index.ts
@@ -0,0 +1,9 @@
+export interface IMajorData {
+    clubid: number;
+    clubname: string;
+    clubtag: string[];
+    clubimage: string;
+    clubdescription: string;
+    clubbanner: string;
+    clubrecruitment: boolean;
+}
