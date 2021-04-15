0206bed1396488c6125a0aa87fd3735f54245caa
oajh04 <cloudtubekr@naver.com>
2021-04-15 22:21:15 +0900

    UPDATE ; ClubView

---
+++ src/components/ClubView/ClubView.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+import ClubPreview from "./ClubPreview/ClubPreview";
+function ClubManagement({ club_id } : { club_id : number}){
+    return(
+        <>
+            <Header color="white"></Header>
+            <ClubPreview club_id={club_id}></ClubPreview>
+        </>
+    )
+}
+export default ClubManagement;
