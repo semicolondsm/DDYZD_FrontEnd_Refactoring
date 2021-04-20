4a76fad00268809ec7ad2cf499743d386aa4db9c
eungyeole <eungyeole@naver.com>
2021-04-20 20:23:12 +0900

    CREATE ; ClubTag

---
+++ src/components/Club/ClubItem/ClubTag/style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+export const FieldIconWrapper = styled.div`
+  position: absolute;
+  top: calc(100% + 15px);
+  right: 0;
+  & svg {
+    margin-left: 5px;
+  }
+`;
