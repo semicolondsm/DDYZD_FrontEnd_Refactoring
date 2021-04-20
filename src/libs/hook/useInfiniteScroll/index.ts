8e23a8541555c70c790d84462b8ac9f97db6bd6e
oajh04 <cloudtubekr@naver.com>
2021-04-20 19:16:30 +0900

    UPDATE ; useInfiniteScroll hook

--- src/libs/hook/useInfiniteScroll/index.ts
+++ src/libs/hook/useInfiniteScroll/index.ts
@@ -1,11 +1,20 @@
 import { AxiosPromise } from 'axios';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, SetStateAction, Dispatch } from 'react';
 
-export function useInfiniteScroll<T>(getData : (page : number) => AxiosPromise<any>): [T[], boolean]{
-  const [data, setData] = useState<T[]>([]);
+interface useInfiniteScrollType {
+  page: number,
+  last: boolean,
+  loading: boolean,
+  setLoading: () => Dispatch<SetStateAction<boolean>>,
+  setPage: () => Dispatch<SetStateAction<boolean>>,
+  setLast: () => Dispatch<SetStateAction<boolean>>,
+}
+
+export function useInfiniteScroll<T>(){
   const [page, setPage] = useState<number>(-1);
   const [last, setLast] = useState<boolean>(false);
   const [loading,setLoading] = useState<boolean>(true);
+
   const scrollEvent = useCallback(()=>{
     let scrollHeight = Math.max(
       document.documentElement.scrollHeight,
@@ -24,20 +33,6 @@ export function useInfiniteScroll<T>(getData : (page : number) => AxiosPromise<a
     window.addEventListener("scroll",scrollEvent)
     return () => window.removeEventListener("scroll",scrollEvent);
   },[])
-  useEffect(()=>{
-    if(loading && !last){
-      setPage(page+1);
-      getData(page+1).then((res)=>{
-        if(res.data.length===0) {
-          setLoading(false);
-          setLast(true);
-        }
-        setData([...data, ...res.data])
-        setLoading(false);
-      })
-      .catch((e)=>console.log(e))
-    }
-    console.log(data)
-  },[loading])
-  return [data, loading];
+
+  return [ page, last, loading];
 }
