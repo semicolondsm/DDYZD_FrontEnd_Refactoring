7e306edcc0bd54c53d40de2fed0eb4556b2a8647
eungyeole <eungyeole@naver.com>
2021-04-20 20:23:42 +0900

    UPDATE ;  ClubItem

--- src/components/Club/ClubItem/index.tsx
+++ src/components/Club/ClubItem/index.tsx
@@ -1,55 +1,27 @@
 import * as S from "./style";
-import earthIc from "@/src/assets/images/earth";
-import phonoeIc from "@/src/assets/images/phone";
-import brainIc from "@/src/assets/images/brain";
-import etcIc from "@/src/assets/images/etc";
-import shieldIc from "@/src/assets/images/shield";
-import embedIc from "@/src/assets/images/embed";
 import Link from "next/link";
 import { CreateRoomAndGetRoomId } from "@/src/libs/function/chat";
 import { useRouter } from "next/router";
-import React, { FC, MouseEvent, useCallback, useEffect, useRef } from "react";
-interface Props {
+import React, { FC, MouseEvent, useCallback } from "react";
+import useSpread from "@/src/libs/hook/useSpread";
+import { IClubData } from "@/src/libs/intefaces/Club";
+import { ClubTag } from "./ClubTag";
+interface Props extends IClubData{
     max: number;
-    now: number;
-    imgSrc?: string;    
-    header: string;
-    tag: string[];
-    description: string;
-    banner: string;
-    id: number;
-    clubrecruitment: boolean;
+    now: number;   
 }
 
-export const ClubItem : FC<Props> = ({imgSrc, header, description, tag, banner, id, max, now, clubrecruitment,}) => {
-    const thisElement=useRef<HTMLDivElement>();
+export const ClubItem : FC<Props> = ({clubbanner, clubdescription, clubid, clubimage, clubname, clubrecruitment, clubtag, now, max}) => {
     const router = useRouter();
-    const time = useRef<ReturnType<typeof setTimeout>>(null);
+    const thisElement = useSpread<HTMLDivElement>(now);
     const convertIdLinkChat = useCallback(async (e : MouseEvent<HTMLElement>) => {
         e.preventDefault();
-        const chat_id: number = await CreateRoomAndGetRoomId(id);
+        const chat_id: number = await CreateRoomAndGetRoomId(clubid);
         if(clubrecruitment) router.push(`/chat/${chat_id}`);
-        else router.push(`/club/${id}`);
-    },[id]);
-    useEffect(()=>{
-        if((now+1)%4===0){
-            const prevElement=thisElement.current.previousElementSibling as HTMLDivElement;
-            thisElement.current.addEventListener("mouseover",()=>{
-                clearTimeout(time.current);
-                thisElement.current.style.zIndex = `40`;
-                prevElement.style.boxShadow="none";
-          
-            })
-            thisElement.current.addEventListener("mouseout", ()=>{
-                prevElement.style.boxShadow = "0 5px 5px rgba(0, 0, 0, 0.29)";
-                time.current=setTimeout(()=>{
-                    thisElement.current.style.zIndex = `0`;
-                },150)
-            })
-        }
-    },[])
+        else router.push(`/club/${clubid}`);
+    },[clubid, router, clubrecruitment]);
     return (
-        <Link href={`/club/${id}`}>
+        <Link href={`/club/${clubid}`}>
             <S.ItemWrapper 
                 ref={thisElement}
                 max={max}
@@ -62,12 +34,12 @@ export const ClubItem : FC<Props> = ({imgSrc, header, description, tag, banner, 
                     <S.PointButton></S.PointButton>
                     <S.ItemImgWrap>
                         <S.ItemImg
-                            src={`${process.env.NEXT_PUBLIC_BASEURL}/file/${banner}`}
+                            src={`${process.env.NEXT_PUBLIC_BASEURL}/file/${clubbanner}`}
                         />
                     </S.ItemImgWrap>
                     <S.ItemFontWrapper>
-                        <S.ItemHeader>{header}</S.ItemHeader>
-                        <S.ItemSubHeader>{description}</S.ItemSubHeader>
+                        <S.ItemHeader>{clubname}</S.ItemHeader>
+                        <S.ItemSubHeader>{clubdescription}</S.ItemSubHeader>
                     </S.ItemFontWrapper>
                     <S.ButtonsWrapper active={clubrecruitment}>
                         <S.RadiusButton onClick={convertIdLinkChat}>
@@ -78,30 +50,13 @@ export const ClubItem : FC<Props> = ({imgSrc, header, description, tag, banner, 
                     </S.ButtonsWrapper>
                     <S.IconWrapper>
                         <S.Icon
-                            src={`${process.env.NEXT_PUBLIC_BASEURL}/file/${imgSrc}`}
+                            src={`${process.env.NEXT_PUBLIC_BASEURL}/file/${clubimage}`}
                         />
                     </S.IconWrapper>
                     <S.IntroWrapper>
-                        <p>{description}</p>
+                        <p>{clubdescription}</p>
                         <S.IntroIcon>
-                            <S.FieldIconWrapper>
-                                {tag.map((val,index) => {
-                                    switch (val) {
-                                        case "인공지능":
-                                            return <span key={index}>{brainIc}</span>;
-                                        case "웹":
-                                            return <span key={index}>{earthIc}</span>;
-                                        case "앱":
-                                            return <span key={index}>{phonoeIc}</span>;
-                                        case "정보보안":
-                                            return <span key={index}>{shieldIc}</span>;
-                                        case "임베디드":
-                                            return <span key={index}>{embedIc}</span>;
-                                        default:
-                                            return <span key={index}>{etcIc}</span>;
-                                    }
-                                })}
-                            </S.FieldIconWrapper>
+                            <ClubTag clubtag={clubtag}></ClubTag>
                         </S.IntroIcon>
                     </S.IntroWrapper>
                 </div>
