ab3ac13522de0f4e322fd6b39d2a0dacae795252
oajh04 <cloudtubekr@naver.com>
2021-04-20 19:15:34 +0900

    UPDATE ; page [clubid]

--- pages/club/[clubid].tsx
+++ pages/club/[clubid].tsx
@@ -1,15 +1,81 @@
-import ClubManagement from "@/src/components/ClubView";
+import { useInfiniteScroll } from "@/src/libs/hook/useInfiniteScroll";
+import { IFeedData } from "@/src/libs/intefaces/Feed";
 import { GetServerSidePropsContext } from "next";
-export function getServerSideProps(context : GetServerSidePropsContext) {
+import { ClubHeader, ClubRecruitment, ClubUtil } from '@/src/components/ClubView';
+import Header from '@/src/components/Header';
+import { useEffect, useState } from "react";
+import { IClubInfo } from "@/src/libs/intefaces/Club";
+import clubAPI from "@/src/libs/api/club";
+import Feed from '@/src/components/Feed';
+
+export const getServerSideProps = (context : GetServerSidePropsContext)  => {
+    
+
     return {
-        props: {clubid: context.query.clubid}
+        props: {
+            clubid: context.query.clubid
+        }
     };
 }
 
+interface infiniteScrollType {
+    page: number,
+    last: boolean,
+    loading: boolean,
+    setLoading: () => boolean,
+}
 
 function club({clubid} : {clubid : number}){
-    return(
-        <ClubManagement club_id={clubid}></ClubManagement>
+    const [ clubData, setClubData ] = useState<IClubInfo>()
+    const [ feedData, setFeedData ] = useState<IFeedData[]>()
+
+    const [ page, last, loading ] = useInfiniteScroll<infiniteScrollType>();
+    const [ feedPage, setFeedPage ] = useState<any>(page);
+    const [ feedLoading, setFeedLoading] = useState<any>(loading)
+    const [ feedLast, setFeedLast] = useState<any>(loading)
+
+    useEffect(()=>{
+        clubAPI.getInfo(clubid)
+        .then((res)=>{
+            setClubData(res.data);
+        })
+    })
+
+    useEffect(()=>{
+        if(loading && !last){
+            setFeedPage(feedPage+1);
+            clubAPI.getFeed(clubid,feedPage+1)
+            .then((res)=>{
+                if(res.data.length===0) {
+                    setFeedLoading(false);
+                setFeedLast(true);
+                }
+                setFeedData([...feedData, ...res.data])
+                setFeedLoading(false);
+            })
+          .catch((e)=>console.log(e))
+        }
+        console.log(feedData)
+      },[])
+    
+    return (
+        <>
+            <Header color="white"/>
+            {
+                clubData ? 
+                <>
+                    <div style={{backgroundColor: "#ffffff"}}>
+                        <ClubHeader data={clubData}></ClubHeader>
+                        <ClubUtil data={clubData}></ClubUtil>
+                    </div>
+                    
+                    <ClubRecruitment club_id={clubid}></ClubRecruitment>
+                    <Feed data={feedData}></Feed>
+                </>
+                : null
+            }
+        </>
     )
 }
+
 export default club;
