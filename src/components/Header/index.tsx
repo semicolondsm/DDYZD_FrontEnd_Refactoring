d339468b092c97af22ed5c1b30df0d9af56788cd
oajh04 <cloudtubekr@naver.com>
2021-04-22 23:32:52 +0900

    UPDATE ; Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,25 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as h from './styles'
 import Link from "next/link"
 import ListIco from './ListIco';
 import ChatIco from './ChatIco';
-import UserContext from '@/src/context/user';
-import { setState } from '@/src/context/context';
+import { useRecoilState } from 'recoil';
+import { userAtom } from '@/src/modules/atom/user';
+
+
 /**야야 */
 function Header({color} : {color:string}){
     const [scroll, setScroll] = useState<number>(0);
-    const { user_state, setUserState } = useContext(UserContext);
-    const [toggle,setToggle] = useState<boolean>(true);
-    const dispatch = setState();
+    const [ userData, setUserData ] = useRecoilState(userAtom);
+
     useEffect(()=>{
         window.onscroll=()=>setScroll(window.scrollY);
-        localStorage.userCache && setUserState(JSON.parse(localStorage.userCache))
+        localStorage.userCache && setUserData(JSON.parse(localStorage.userCache))
     },[])
     const logout =()=>{
         alert("로그아웃 되었습니다.")
         localStorage.clear()
         window.location.href="/"  
-    }
-    
-    const ModalOn=()=>{
-        setToggle(!toggle)
-        if(toggle) dispatch({type:"SET_MODAL",name:"mainModal"})
-        else dispatch({type:"SET_MODAL",name:""})
     }
 
     return(
@@ -40,8 +35,8 @@ function Header({color} : {color:string}){
             </h.TopHeader>
             <h.BottomHeader>
                 <ul>
-                    <div onClick={ModalOn} style={{cursor:"pointer"}}><ListIco></ListIco></div>
-                    <h3 onClick={ModalOn} style={{cursor:"pointer"}}>전체 카테고리</h3>
+                    <div style={{cursor:"pointer"}}><ListIco></ListIco></div>
+                    <h3 style={{cursor:"pointer"}}>전체 카테고리</h3>
                     <li><Link href="/majorlist"><a>동아리 신청</a></Link></li>
                     <li onClick={()=>alert("개발중")}>동아리 물품 신청</li>
                     <li onClick={()=>window.location.href="https://ddyzd-club.dsmkr.com/"}>관리 페이지</li>
@@ -49,8 +44,8 @@ function Header({color} : {color:string}){
                 </ul>
                 <ul>
                     {
-                        user_state?
-                            <><li><Link href={`/profile?id=${user_state.gcn}`}><a>{user_state?.name}</a></Link></li><li onClick={logout}>로그아웃</li></>
+                        userData?
+                            <><li><Link href={`/profile?id=${userData.gcn}`}><a>{userData?.name}</a></Link></li><li onClick={logout}>로그아웃</li></>
                         :<li onClick={()=>window.location.href="/login"}>로그인</li>
                     }
                     <li>고객센터</li>
