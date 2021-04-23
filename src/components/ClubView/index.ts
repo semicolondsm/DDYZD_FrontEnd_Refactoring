3efa8199f607c33149c499594ea5af30bdcd2deb
oajh04 <cloudtubekr@naver.com>
2021-04-24 03:07:25 +0900

    UPDATE ; 클럽멤버 인덱스

--- src/components/ClubView/index.ts
+++ src/components/ClubView/index.ts
@@ -22,3 +22,4 @@
 export { ClubHeader } from './ClubHeader';
 export { ClubRecruitment } from './ClubRecruitment';
 export { ClubUtil } from './ClubUtil';
+export { ClubMember } from './ClubMember';
