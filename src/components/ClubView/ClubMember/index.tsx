a763a52a0aac3f8e03c58301df4a0372ce87b1b8
oajh04 <cloudtubekr@naver.com>
2021-04-24 03:04:37 +0900

    CREATE ; 클럽멤버

---
+++ src/components/ClubView/ClubMember/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC, Fragment, useEffect, useState } from "react";
+import * as S from "./styles";
+import club from "@/src/libs/api/club";
+import { IClubInfo, IMemberData } from "@/src/libs/intefaces/Club";
+import { MemberSkeleton } from "./MemberSkeleton";
+
+interface Props{
+  data: IClubInfo
+}
+
+export const ClubMember: FC<Props> = ({data}) => {
+  const [memberData, setMemberData] = useState<IMemberData[]>(null);
+
+  useEffect(() => {
+    club.getMembers(data.clubid).then((res) => {
+      setMemberData(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      {
+        data && memberData ? 
+        <S.ClubMemberContainer>
+          <S.MemberHeader>
+            <div>{data.clubname} 동아리원</div>
+            <div>인원수: {memberData.length}명</div>
+          </S.MemberHeader>
+          <S.MemberContainer>
+            {memberData.map((data, index) => {
+              return (
+                (
+                  <S.Member key={index}>
+                    <S.Img src={data.profile_image} />
+                    <div>
+                      <S.MemberName>{data.user_name}</S.MemberName>
+                      <S.MemberRole>{index === 0 ? "동아리장" : "동아리원"}</S.MemberRole>
+                    </div>
+                  </S.Member>
+                )
+              );
+            })}
+          </S.MemberContainer>
+          <S.Dummy/>
+        </S.ClubMemberContainer>
+      : <MemberSkeleton />
+      }
+    </>
+  );
+}
