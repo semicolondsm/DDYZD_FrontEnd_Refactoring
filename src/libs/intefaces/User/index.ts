80bf44fe26a84b4fc38efea29aefe69c510a0edc
eungyeole <eungyeole@naver.com>
2021-04-24 03:06:10 +0900

    UPDATE; <libs/Interfaces> User Interface

--- src/libs/intefaces/User/index.ts
+++ src/libs/intefaces/User/index.ts
@@ -1,4 +1,13 @@
-export type IUser = {
-    id: number;
-    name: string;
+import { IClub } from "../Club";
+
+
+export interface IUser{
+    bio: string | null,
+    clubs: IClub[],
+    email: string,
+    gcn: string,
+    github_url: string | null,
+    id: number,
+    image_path: string | null,
+    name: string
 };
