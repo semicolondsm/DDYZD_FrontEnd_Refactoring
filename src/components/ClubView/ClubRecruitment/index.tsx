fbe6444b2577a8a7609728ee9565f88ea9e56650
oajh04 <cloudtubekr@naver.com>
2021-04-24 16:28:38 +0900

    UPDATE ; ClubRecuritment

--- src/components/ClubView/ClubRecruitment/index.tsx
+++ src/components/ClubView/ClubRecruitment/index.tsx
@@ -4,12 +4,15 @@ import * as S from "./styles"
 import yyyymmddFormat from "@/src/libs/function/yyyymmddFormat";
 import getDday from "@/src/libs/function/getDday";
 import { IRecruitmentData } from "@/src/libs/intefaces/Club";
+import { CreateRoomAndGetRoomId } from "@/src/libs/function/chat";
+import { useRouter } from "next/router"
 
 interface Props {
     club_id: number
 }
 
 export const ClubRecruitment: FC<Props> = ({club_id}) =>{
+    const router = useRouter();
     const [data,setData] = useState<IRecruitmentData>();
 
     useEffect(()=>{
@@ -18,29 +21,49 @@ export const ClubRecruitment: FC<Props> = ({club_id}) =>{
         .catch((e)=>console.error(e))
     },[])
 
+    const onSupport = async () => {
+        if (!localStorage.getItem("accessToken")) {
+            alert("로그인을 해주세요 !");
+            return;
+        }
+        const chat_id = await CreateRoomAndGetRoomId(
+            Number(router.query.clubid)
+        );
+        router.push("/chat/" + chat_id);
+    };
+
     return(
         <>
             {
                 data ? 
                 <S.Wrapper>
-                    <div>
-                        <S.HeaderWrapper>
-                            <p>모집분야</p>
-                            
-                        </S.HeaderWrapper>
+                    <S.HeaderWrapper>
+                        동아리 지원
+                    </S.HeaderWrapper>
+
+                    <S.MajorWrapper>
+                        모집분야
                         <S.TagList>
                             {
                                 data?.major.map((i, index)=>(<span key={index}>{i}</span>))
                             }
                         </S.TagList>
-                        <p>모집기간</p>
-                        <S.RecuitmentDay>
+                    </S.MajorWrapper>
+                
+                    <S.DdayWapper>
+                        모집기간
+                        <S.Dday>
+                            <div style={{alignItems: "center", width: '20px', height: '20px', border: "1px solid black", borderRadius: "50%", marginRight: "10px"}}></div>
                             {data && yyyymmddFormat(data.startat)}
                             ~  
                             {data && yyyymmddFormat(data.closeat)} 
                             ( {data && getDday(data.startat, data.closeat)}일간 )
-                        </S.RecuitmentDay>
-                    </div>
+                        </S.Dday>
+                    </S.DdayWapper>
+
+                    <S.ButtonWapper>
+                        <S.Button onClick={onSupport}>지원하기</S.Button>
+                    </S.ButtonWapper>
                 </S.Wrapper>
                 : null
             }
