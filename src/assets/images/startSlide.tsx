db97c2cc414d52c78d1198a0bc6f749080f018de
oajh04 <cloudtubekr@naver.com>
2021-04-28 22:02:18 +0900

    CREATE; startSlide 이미지

---
+++ src/assets/images/startSlide.tsx
@@ -0,0 +1,8 @@
+export default(
+    <svg id="구성_요소_12_1" data-name="구성 요소 12 – 1" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35">
+        <circle id="타원_83" data-name="타원 83" cx="17.5" cy="17.5" r="17.5" fill="#3d3d3d" opacity="0"/>
+        <path id="다각형_1" data-name="다각형 1" d="M6.652,1.357a1,1,0,0,1,1.7,0l5.7,9.113A1,1,0,0,1,13.2,12H1.8a1,1,0,0,1-.848-1.53Z" transform="translate(24.6 9.875) rotate(90)" fill="#fff"/>
+        <rect id="사각형_591" data-name="사각형 591" width="4" height="12" rx="1" transform="translate(12 11)" fill="#fff" opacity="0"/>
+        <rect id="사각형_592" data-name="사각형 592" width="4" height="12" rx="1" transform="translate(19 11)" fill="#fff" opacity="0"/>
+    </svg>
+)
